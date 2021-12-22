1df14da429546881a18c4df68fb123b29123d22c
Szymon Kolakowski <szymon.kolakowski@gmail.com>
2021-12-22 13:52:18 +0100

    #3 push [using script]

--- bot_engine.py
+++ bot_engine.py
@@ -1,6 +1,8 @@
 import discord
 import os
+from stockfish import Stockfish
 
+stockfish = Stockfish()
 client = discord.Client()
 
 @client.event
@@ -11,7 +13,27 @@ async def on_ready():
 async def on_message(message):
 	if message.author == client.user:
 		return
-	else:
-		await message.channel.send('alfa')
+	if message.content.startswith('!fen'):
+		fen = str(message.content)[5:]
+		print(fen)
+		stockfish.set_fen_position(fen)
+		bm = stockfish.get_best_move()
+		stockfish.make_moves_from_current_position([bm])
+		bv = stockfish.get_board_visual()
+		print(bv)
+		await message.channel.send(bm + '\n```bash\n' + bv + '```')
+	if message.content.startswith('!pic'):
+		cont = str(message.content).split(' ')
+		if len(cont) != 3:
+			await message.channel.send('Wrong arguments given for !pic function!')
+			await message.channel.send('!pic <can white castle? (0 / 1)> <can black castle? (0 / 1)> <who moves now? (0 / 1)>')
+			await message.channel.send('0 = false = black')
+			await message.channel.send('1 = true = white')
+			return
+		try:
+			string = message.attachments[0]
+			await message.channel.send(string)
+		except FileNotFoundError:
+			return
 
-client.run('OTIxNTI2MzU1NjEzNTg1NDE4.Yb0MTA.1I2896D1L-8_Co_q0I7p-mN_ya') # add I
+client.run('OTIxNTI2MzU1NjEzNTg1NDE4.Yb0MTA.1I2896D1L-8_Co_q0I7p-mN_yaI') # add I
